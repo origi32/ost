b034d8d4c2aa706e6a981ab1f3f6e69ced850e77
origi32 <68254224+origi32@users.noreply.github.com>
2020-07-19 13:50:18 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,113 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+
+//???????????????????????????????????????????????????????????????????????????????
+
+client.on("ready", () => {
+let channel =     client.channels.get("id")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+client2.on("ready", () => {
+let channel =     client2.channels.get("id")
+setInterval(function() {
+channel.send(`عبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللةعبداللة`);
+}, 30)
+})
+
+client3.on("ready", () => {
+let channel =     client3.channels.get("id")
+setInterval(function() {
+channel.send(`الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+client4.on("ready", () => {
+let channel =     client4.channels.get("id")
+setInterval(function() {
+channel.send(`الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+client5.on("ready", () => {
+let channel =     client5.channels.get("id")
+setInterval(function() {
+channel.send(`الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+client6.on("ready", () => {
+let channel =     client6.channels.get("id")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+client7.on("ready", () => {
+let channel =     client7.channels.get("id")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+client8.on("ready", () => {
+let channel =     client8.channels.get("id")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+client9.on("ready", () => {
+let channel =     client9.channels.get("id")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+clien10t.on("ready", () => {
+let channel =     client10.channels.get("id1")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+client11.on("ready", () => {
+let channel =     client11.channels.get("id")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+client12.on("ready", () => {
+let channel =     client12.channels.get("id")
+setInterval(function() {
+channel.send(`محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد محمد`);
+}, 30)
+})
+
+
+client.login(process.env.T);
+client2.login(process.env.T2);
+client3.login(process.env.T3);
+client4.login(process.env.T4);
+client5.login(process.env.T5);
+client6.login(process.env.T6);
+client7.login(process.env.T7);
+client8.login(process.env.T8);
+client9.login(process.env.T9);
+client10.login(process.env.T10);
+client11.login(process.env.T11);
+client12.login(process.env.T12);
